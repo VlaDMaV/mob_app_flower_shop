001c46d777a8a109e8cd670ca80f10be506cfb2d
VlaDMaV <makeev.vlados.9.2005@gmail.com>
2025-04-19 02:23:01 +0300

    1

--- app/src/main/java/com/example/flowershopapp/MainActivity.kt
+++ app/src/main/java/com/example/flowershopapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FlowerShopAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hi",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
